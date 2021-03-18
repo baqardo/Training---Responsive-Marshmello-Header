fae01587123c01d43751da5aaf9a7af3bb68e8a1
Andrzej Nawalaniec <46224560+baqardo@users.noreply.github.com>
2021-03-18 19:51:50 +0100

    Responsive navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,39 +1,100 @@
-import React from "react";
+import React, { Component } from "react";
 
 import logo from "../../assets/logo.png";
 
 import "./Navigation.scss";
+class Navigation extends Component {
+  constructor(props) {
+    super(props);
 
-const Navigation = () => (
-  <nav className="nav">
-    <ul className="nav__list">
-      <li className="nav__item nav__item--logo">
-        <a href="#self">
-          <img className="nav__logo" src={logo} alt="logo" width="40px" height="33px" />
-        </a>
-      </li>
-      <li className="nav__item">
-        <a className="nav__link nav__link--current-page" href="#self">
-          Upcoming show
-        </a>
-      </li>
-      <li className="nav__item">
-        <a hre className="nav__link" href="#self">
-          Media
-        </a>
-      </li>
-      <li className="nav__item">
-        <a hre className="nav__link" href="#self">
-          Songs
-        </a>
-      </li>
-      <li className="nav__item">
-        <a className="nav__link" href="#self">
-          Merchandise
-        </a>
-      </li>
-    </ul>
-  </nav>
-);
+    this.state = { isMobileNav: false };
+    this.mobileNavLinkRef = React.createRef();
+
+    this.mobileNavTemplate = (
+      <nav className="nav-mobile">
+        <ul className="nav__list nav__list--mobile">
+          <li className="nav__item nav__item--mobile">
+            <a className="nav__link nav__link--mobile nav__link--current-page" href="#self" ref={this.mobileNavLinkRef}>
+              Upcoming show
+            </a>
+          </li>
+          <li className="nav__item nav__item--mobile">
+            <a className="nav__link nav__link--mobile" href="#self" onClick={this.toggleMobileNav}>
+              Media
+            </a>
+          </li>
+          <li className="nav__item nav__item--mobile">
+            <a className="nav__link nav__link--mobile" href="#self" onClick={this.toggleMobileNav}>
+              Songs
+            </a>
+          </li>
+          <li className="nav__item nav__item--mobile">
+            <a className="nav__link nav__link--mobile" href="#self" onClick={this.toggleMobileNav}>
+              Merchandise
+            </a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+
+  componentDidUpdate() {
+    if (!this.state.isMobileNav) return;
+
+    this.mobileNavLinkRef.current.focus();
+  }
+
+  toggleMobileNav = () => {
+    this.setState(state => {
+      return {
+        isMobileNav: !state.isMobileNav,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <>
+        {this.state.isMobileNav && this.mobileNavTemplate}
+        <nav className="nav">
+          <ul className="nav__list">
+            <li className="nav__item nav__item--logo">
+              <a className="nav__link nav__link--logo" href="#self">
+                <img className="nav__logo" src={logo} alt="logo" />
+              </a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link nav__link--current-page" href="#self">
+                Upcoming show
+              </a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link" href="#self">
+                Media
+              </a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link" href="#self">
+                Songs
+              </a>
+            </li>
+            <li className="nav__item">
+              <a className="nav__link" href="#self">
+                Merchandise
+              </a>
+            </li>
+
+            <li className="nav__item nav__item--burger">
+              <a className="nav__burger" href="#self" onClick={this.toggleMobileNav}>
+                <div className="nav__burger-line"></div>
+                <div className="nav__burger-line nav__burger-line--small"></div>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </>
+    );
+  }
+}
 
 export default Navigation;
